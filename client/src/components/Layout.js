88014b68c9d697e06b5a8bc39c88b709b059e03d
Getachew0557 <getachewgetu2010@gmail.com>
2025-01-30 14:37:10 +0300

    side-bar implemented

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, { use } from 'react'
 import '../Layout.css'
+import { Link, useLocation } from 'react-router-dom';
 
 
 function Layout({children}) {
+ const location = useLocation()
+ //define the menu items
+ const userMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: 'ri-home-line'   
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+        icon: 'ri-file-list-line'
+    },
+    {
+      name: "Patients",
+      path: "/patients",
+        icon: 'ri-user-2-line'
+    },
+    {
+      name: "Doctors",  
+      path: "/doctors",
+        icon: 'ri-nurse-line'    
+    },
+    {
+        name: "Profile",
+        path: "/profile",
+        icon: 'ri-profile-line'
+    },
+
+    {
+      name: "Logout",
+      path: "/logout",
+        icon: 'ri-logout-circle-line'
+    },
+
+ ];
+ const menuToBeRendered = userMenu
   return (
     <div className='main'>
         <div className='d-flex layout'>
             <div className='sidebar'>
-                sidebar
+                <div className='sidebar-header'>
+                    <h1>HA</h1>
+                </div>
+                <div className='menu'>
+                    {
+                        menuToBeRendered.map((menu) => {
+                            const isActive = location.pathname === menu.path
+                            return (
+                                <div className={`d-flex menu-item ${isActive && 'active-menu-item'}`} >
+                                    <i className={menu.icon}></i>
+                                    <Link to={menu.path}>{menu.name}</Link>
+                                </div>
+                            )
+
+                        })
+                        } 
+
+                </div>
+
 
             </div>
 
